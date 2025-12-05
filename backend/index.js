481292bd0ef03fa927512208cc25e60f4c0293bc
sonu <sonu9964@gmail.com>
2025-12-05 20:03:44 +0530

    this is first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors()); // to avoid CORS error this is middleware to allow cross-origin requests
+
+app.use(express.json());
+const PORT = 5000;
+app.get('/', (req, res) => {
+    // res.send('Hello from backend');
+    res.json({ message: 'Hello from backend' });
+});
+app.listen(PORT,()=>{
+    console.log(`server is running on the port ${PORT}`);
+})
